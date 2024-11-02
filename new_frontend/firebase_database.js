57747fec8353135d5975937e0010112c4e4c7841
jbaron2025 <jbaron@seattleu.edu>
2024-11-01 18:18:25 -0700

    added database connection

---
+++ new_frontend/firebase_database.js
@@ -0,0 +1,54 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDG7rEfTtegzIBHFvL6W2rV7HNmmMlkNcQ",
+    authDomain: "sesdc-micro-design-tool.firebaseapp.com",
+    projectId: "sesdc-micro-design-tool",
+    storageBucket: "sesdc-micro-design-tool.firebasestorage.app",
+    messagingSenderId: "99363626334",
+    appId: "1:99363626334:web:6aaa35b53358a235fc43cf",
+    measurementId: "G-7DMVG3X5Y6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+import {getDatabase, set, get, update, remove, ref, child}
+from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js"
+
+const db = getDatabase(app);
+
+document.addEventListener("DOMContentLoaded", function() {
+    var insertBtn = document.querySelector("#login");
+    insertBtn.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent default behavior of the button
+        loginData().then(() => {
+        // Navigate to projects.html after successful login
+        window.location.href = "projects.html";
+        }).catch(error => {
+            console.error("Login failed:", error);
+        });
+    });
+});
+
+function loginData() {
+    var username = document.querySelector("#enterUsername");
+    var password = document.querySelector("#enterPassword"); 
+
+    return set(ref(db, "People/" + username.value), {
+        Username: enterUsername.value,
+        Password: enterPassword.value,
+    })
+    .then(()=>{
+        alert("Data added Successfully!")
+    })
+    .catch(()=>{
+        alert(error)
+    } )
+}
+
