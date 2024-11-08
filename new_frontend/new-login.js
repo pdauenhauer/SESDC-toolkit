0cc6de4daac24e665085e87fab73cc6dd02ef104
jbaron2025 <jbaron@seattleu.edu>
2024-11-08 08:13:39 -0800

    store user data in database and created account creation message

--- new_frontend/new-login.js
+++ new_frontend/new-login.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
+import { getFirestore, setDoc, doc } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -19,6 +19,13 @@ const db = getFirestore(app);
 const loginForm = document.getElementById('loginForm');
 const registerForm = document.getElementById('registerForm');
 
+function showMessage(message, divId) {
+    var messageDiv = document.getElementById(divId);
+    messageDiv.textContent = message;
+    messageDiv.style.display = 'block';
+    messageDiv.style.animation = 'fade-out 5s forwards';
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const signupForm = document.getElementById('signup-form');
     signupForm.addEventListener("submit", (e) => {
@@ -26,10 +33,33 @@ document.addEventListener('DOMContentLoaded', () => {
         if (registerForm.classList.contains('visible')) {
             const email = signupForm['enterEmail'].value;
             const password = signupForm['enterPassword'].value;
+            const username = signupForm['enterUsername'].value;
 
             createUserWithEmailAndPassword(auth, email, password).then(cred => {
-                showForm(loginForm, registerForm);
-            });
+                const user = cred.user;
+                const userData = {
+                    username: username,
+                    email: email,
+                    password: password,
+                };
+                showMessage('Account Created Successfully!', 'account-creation-message');
+                const docRef = doc(db, "users", user.uid);
+                setDoc(docRef, userData).then(() => {
+                    showForm(loginForm, registerForm);
+                })
+                .catch((error) => {
+                    console.error("error writing document", error);
+                });
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                if (errorCode =='auth/email-already-in-use') {
+                    showMessage('Email already Exists', 'account-creation-message');
+                }
+                else {
+                    showMessage('Unable to Create User', 'account-creation-message');
+                }
+            });   
         }
     });
 });
