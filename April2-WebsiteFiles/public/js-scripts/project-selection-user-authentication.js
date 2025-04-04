a67a6cdbfedb3f206fcab7e6ce4ac7580b980f2e
Jessica Huang <huangjessica03@gmail.com>
2025-04-03 17:50:59 -0700

    Added logo

---
+++ April2-WebsiteFiles/public/js-scripts/project-selection-user-authentication.js
@@ -0,0 +1,43 @@
+import { app } from './firebase-init.js';
+import { getAuth, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
+import { getFirestore, getDoc, doc } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+window.addEventListener('load', (event) => {
+    if (localStorage.getItem('loggedInUserId') === null) {
+        window.location.href = "new-login.html";
+    }
+});
+
+onAuthStateChanged(auth, (user) => {
+    const loggedInUserId = localStorage.getItem('loggedInUserId');
+    if (loggedInUserId) {
+        const usersRef = doc(db, "users", loggedInUserId);
+        getDoc(usersRef).then((docSnap) => {
+        if(docSnap.exists()) {
+            const userData = docSnap.data();
+            document.getElementById('logged-in-username').innerText = userData.username;
+        } else {
+            console.log("No document found matching id");
+        }
+        })
+        .catch ((error) => {
+            console.log("Error getting document");
+        })
+    } else {
+        console.log("UserId not found in local storage");
+    }
+});
+
+const logoutButton = document.getElementById('logout');
+logoutButton.addEventListener("click", () => {
+    localStorage.removeItem('loggedInUserId');
+    signOut(auth).then(() => {
+        window.location.href = 'new-login.html';
+    })
+    .catch((error) => {
+        console.error('Error Signing Out', error);
+    })
+})
