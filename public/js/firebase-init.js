df5826fc59a27c47c2df145ec56eff04b4c96588
jbaron2025 <121137563+jbaron2025@users.noreply.github.com>
2025-06-27 16:37:35 -0700

    Update firebase-init.js

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -2,7 +2,12 @@ import { initializeApp, getApps } from 'https://www.gstatic.com/firebasejs/9.22.
 import { getStorage } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js';
 
 const firebaseConfig = {
-    // Need to add your Firebase configuration here
+    apiKey: "AIzaSyDa0W1i5K_3tnh26xUpO9xTEIQDeHgpnrA",
+    authDomain: "sesdc-toolkit2.firebaseapp.com",
+    projectId: "sesdc-toolkit2",
+    storageBucket: "sesdc-toolkit2.firebasestorage.app",
+    messagingSenderId: "987889774022",
+    appId: "1:987889774022:web:a36f983b38c8d558c4d6c7"    
 };
 
 let app;
