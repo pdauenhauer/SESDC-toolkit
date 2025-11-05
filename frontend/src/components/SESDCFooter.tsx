5914aa56386b489b34f0924c8ccccdb4e731f331
Bryan Kim <wabkim360@gmail.com>
2025-11-04 23:26:23 -0800

    added header and footer as components

---
+++ frontend/src/components/SESDCFooter.tsx
@@ -0,0 +1,26 @@
+function SESDCFooter() {
+    return (
+        <footer class="footer">
+            <div class="social-icons">
+            <a href="https://www.facebook.com/KilowattsforHumanity/" target="_blank">
+                <i class="bx bxl-facebook"></i>
+            </a>
+            <a href="https://www.instagram.com/kilowattsforhumanity/" target="_blank">
+                <i class="bx bxl-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/company/kilowatts-for-humanity/posts/?feedView=all" target="_blank">
+                <i class="bx bxl-linkedin"></i>
+            </a>
+            <a href="https://github.com/pdauenhauer/SESDC-toolkit" target="_blank">
+                <i class="bx bxl-github"></i>
+            </a>
+            </div>
+
+            <div class="footer-bottom">
+                <p>&copy; 2025 SESDC. All rights reserved</p>
+            </div>
+        </footer>
+    )
+}
+
+export default SESDCFooter
