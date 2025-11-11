53f8393d00bd3f8dbaab1816f83e054ef1f608b9
Bryan Kim <wabkim360@gmail.com>
2025-11-10 16:00:08 -0800

    changed homepage to Home

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,5 +1,3 @@
-import './app.css'
-import './guide.css'
 import { LocationProvider, ErrorBoundary, Router, Route } from 'preact-iso'
 import About from './pages/About'
 import Home from './pages/Home'
@@ -7,13 +5,14 @@ import Contact from './pages/Contact'
 import Account from './pages/Account'
 import UserGuide from './pages/UserGuide'
 import Login from './pages/Login'
+import './app.css'
 
 export function App() {
   return (
     <LocationProvider>
       <ErrorBoundary onError={e => alert(e)}> 
         <Router>
-          <Route path="/" component={Login}/>
+          <Route path="/" component={Home}/>
           <Route path="/about" component={About}/>
           <Route path="/contact" component={Contact}/>
           <Route path="/account" component={Account}/>
