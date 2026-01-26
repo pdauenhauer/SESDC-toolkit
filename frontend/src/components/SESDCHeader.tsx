fcf40760257f502badcc9581f404029660de20b6
Bryan Kim <103244689+hiimkimchi@users.noreply.github.com>
2026-01-26 12:51:45 -0800

    account page update (#17)
    * move delete logic to auth
    
    * import
    
    * version dependency fix
    
    * slight revamp of account page (PLEASE move everything to tailwind later)
    
    * remove logout from header
    
    * make the box more lively

--- frontend/src/components/SESDCHeader.tsx
+++ frontend/src/components/SESDCHeader.tsx
@@ -38,9 +38,8 @@ function SESDCHeader() {
                         <li><a className={isActive('/projects')} href="/projects">Projects</a></li>
                         <li><a className={isActive('/about')} href="/about">About</a></li>
                         <li><a className={isActive('/contact')} href="/contact">Contact</a></li>
-                        <li><a className={isActive('/account')} href="/account">Account</a></li>
                         <li><a className={isActive('/guide')} href="/guide">User Guide</a></li>
-                        <li><a className={isActive('/logout')} href="/logout">Logout</a></li>
+                        <li><a className={isActive('/account')} href="/account">Account</a></li>
                     </>
                 ) : (
                     <>
